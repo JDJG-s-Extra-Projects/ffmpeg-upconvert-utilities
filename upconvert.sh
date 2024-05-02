852a497b542c2dbc907d3105ebdc7934bc87d69e
cyrus01337 <cyrus21337@gmail.com>
2024-05-02 08:52:10 +0100

    Transition back to previous configuration while using Docker

--- upconvert.sh
+++ upconvert.sh
@@ -12,23 +12,31 @@ else
 fi
 
 for source in "$@"; do
-    base=$(basename "$source")
+    base="$(basename $source)"
     target="${base}-upgraded.mp4"
+    quality="${base}-quality.mp4"
 
     if [ "$is_nvidia" = true ]; then
         docker run --rm -it \
             --runtime=nvidia \
             -v $(pwd):/config \
             linuxserver/ffmpeg \
-            -hwaccel nvdec \
             -i "/config/$base" \
-            -c:v h264_nvenc \
-            -b:v 8M \
-            -vtag hvc1 \
-            -vf scale=1920:1080 \
-            -preset fast \
-            -c:a copy \
+            -c:v hevc_nvenc \
+            -qp 17 \
+            -vf scale=1920:1080:flags=lanczos \
             "/config/$target"
+
+        docker run --rm -it \
+            --runtime=nvidia \
+            -v $(pwd):/config \
+            linuxserver/ffmpeg \
+            -i "/config/$source" \
+            -i "/config/$target" \
+            -c:v hevc_nvenc \
+            -qp 17 \
+            -filter_complex "[0]pad=iw:1080:0:(1080-ih)/2,hstack" \
+            "/config/$quality"
     elif [ "$is_amd" = true ]; then
         docker run --rm -it \
             --device=/dev/dri:/dev/dri \
@@ -37,12 +45,8 @@ for source in "$@"; do
             -vaapi_device /dev/dri/renderD128 \
             -i "/config/$base" \
             -c:v hevc_vaapi \
-            -b:v 8M \
-            -vtag hvc1 \
+            -qp 17 \
             -vf "format=nv12,hwupload,scale_vaapi=w=1920:h=1080" \
-            -crf 20 \
-            -preset fast \
-            -c:a copy \
             "/config/$target"
     fi
 done
