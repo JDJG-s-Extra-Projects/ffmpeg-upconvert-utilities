da38028c586c662e92a73b1fd6be085779da61e2
JDJG <167665838+JDJG3493@users.noreply.github.com>
2024-05-01 20:57:40 -0400

    Update upconvert.sh
    Credit to @Motzumoto
    Thank you.

--- upconvert.sh
+++ upconvert.sh
@@ -2,14 +2,10 @@
 
 for SOURCE in "$@"
 do
-    BASE="${SOURCE%.*}"
+    BASE=$(basename "$SOURCE")
     TARGET="${BASE} upgraded.mp4"
     QUALITY="${BASE} quality.mp4"
 
-    echo "time to upconvert the video"
-    ffmpeg -i "${SOURCE}"-c:v hevc_nvenc -qp 17 -vf scale=1920x1080:flags=lanczos "${TARGET}"
-    echo "finished upconverting"
-    echo "comparasion time"
-    ffmpeg -i "${SOURCE}" -i "${TARGET}" -c:v hevc_nvenc -qp 17 -filter_complex "[0]pad=iw:1080:0:(1080-ih)/2,hstack" "${QUALITY}"
-    echo "done generating"
+    ffmpeg -i "$SOURCE" -c:v hevc_nvenc -qp 17 -vf scale=1920x1080:flags=lanczos "$TARGET"
+    ffmpeg -i "$SOURCE" -i "$TARGET" -c:v hevc_nvenc -qp 17 -filter_complex "[0]pad=iw:1080:0:(1080-ih)/2,hstack" "$QUALITY"
 done
