38b4ce8211039c5f539d656351f3d10f55c7774e
JDJG <167665838+JDJG3493@users.noreply.github.com>
2024-05-01 21:00:10 -0400

    Update upconvert.sh
    Credit to @motzumoto
    Thank you for the updated copy.

--- upconvert.sh
+++ upconvert.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+# Exit on error
+set -e
 
-for SOURCE in "$@"
+# Check if ffmpeg is installed
+if ! command -v ffmpeg &> /dev/null
+then
+    echo "ffmpeg could not be found"
+    exit
+fi
+
+# Loop over all input arguments
+for source in "$@"
 do
-    BASE=$(basename "$SOURCE")
-    TARGET="${BASE} upgraded.mp4"
-    QUALITY="${BASE} quality.mp4"
+    # Get the base name of the source file
+    base=$(basename "$source")
+    target="${base} upgraded.mp4"
+    quality="${base} quality.mp4"
 
-    ffmpeg -i "$SOURCE" -c:v hevc_nvenc -qp 17 -vf scale=1920x1080:flags=lanczos "$TARGET"
-    ffmpeg -i "$SOURCE" -i "$TARGET" -c:v hevc_nvenc -qp 17 -filter_complex "[0]pad=iw:1080:0:(1080-ih)/2,hstack" "$QUALITY"
+    # Convert the source file to hevc format and scale it
+    ffmpeg -i "$source" -c:v hevc_nvenc -qp 17 -vf scale=1920x1080:flags=lanczos "$target"
+
+    # Create a quality version of the source file
+    ffmpeg -i "$source" -i "$target" -c:v hevc_nvenc -qp 17 -filter_complex "[0]pad=iw:1080:0:(1080-ih)/2,hstack" "$quality"
 done
