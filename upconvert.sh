bc9b03fa1604983a6b3e7bb3ed2f96199c646f21
cyrus01337 <cyrus21337@gmail.com>
2024-05-03 12:46:12 +0100

    Refactor

--- upconvert.sh
+++ upconvert.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-args="$@"
+ARGUMENTS="$@"
+DIRECTORY_NAME="config"
+DIRECTORY="/$DIRECTORY_NAME"
+VOLUME="$PWD:$DIRECTORY"
 is_nvidia=false
 is_amd=false
 
@@ -11,43 +14,74 @@ else
     is_amd=true
 fi
 
-for source in "$@"; do
-    base="$(basename $source)"
-    filename="${base%.*}"
-    target="${filename}-upgraded.mp4"
-    quality="${filename}-quality.mp4"
+hevc_upscale() {
+    input_file="$(basename $1)"
+    input_file_without_extension="${input_file%.*}"
+    input="$DIRECTORY/$input_file"
+
+    output_file="$(basename $2)"
+    output_file_without_extension="${output_file%.*}"
+    output="$DIRECTORY/$output_file_without_extension.mp4"
 
     if [ "$is_nvidia" = true ]; then
         docker run --rm -it \
             --runtime=nvidia \
-            -v $(pwd):/config \
+            -v $VOLUME \
             linuxserver/ffmpeg \
-            -i "/config/$base" \
+            -i $input \
             -c:v hevc_nvenc \
             -qp 17 \
             -vf scale=1920:1080:flags=lanczos \
-            "/config/$target"
-
-        docker run --rm -it \
-            --runtime=nvidia \
-            -v $(pwd):/config \
-            linuxserver/ffmpeg \
-            -i "/config/$base" \
-            -i "/config/$target" \
-            -c:v hevc_nvenc \
-            -qp 17 \
-            -filter_complex "[0]pad=iw:1080:0:(1080-ih)/2,hstack" \
-            "/config/$quality"
+            $output
     elif [ "$is_amd" = true ]; then
         docker run --rm -it \
             --device=/dev/dri:/dev/dri \
-            -v $(pwd):/config \
+            -v $VOLUME \
             linuxserver/ffmpeg \
             -vaapi_device /dev/dri/renderD128 \
-            -i "/config/$base" \
+            -i $input \
             -c:v hevc_vaapi \
             -qp 17 \
             -vf "format=nv12,hwupload,scale_vaapi=w=1920:h=1080" \
-            "/config/$target"
+            $output
     fi
+}
+
+comparison_render() {
+    source_file="$(basename $1)"
+    source_file_without_extension="${source_file%.*}"
+    comparison_file="$(basename $2)"
+    comparison_file_without_extension="${comparison_file%.*}"
+    input_flags="-i $DIRECTORY/$source_file -i $DIRECTORY/$comparison_file"
+
+    last_argument="${!#}"
+    output_file="$(basename $last_argument)"
+    output_file_without_extension="${output_file%.*}"
+    output="$DIRECTORY/$output_file_without_extension.mp4"
+
+    echo $input_flags
+
+    if [ "$is_nvidia" = true ]; then
+        docker run --rm -it \
+            --runtime=nvidia \
+            -v $VOLUME \
+            linuxserver/ffmpeg \
+            $input_flags \
+            -c:v hevc_nvenc \
+            -qp 17 \
+            -filter_complex "[0]pad=iw:1080:0:(1080-ih)/2,hstack" \
+            $output
+    elif [ "$is_amd" = true ]; then
+        echo "Comparison rendering is unsupported for AMD"
+    fi
+}
+
+for source in $ARGUMENTS; do
+    base="$(basename $source)"
+    filename="${base%.*}"
+    upscaled="${filename}-upscaled.mp4"
+    final="${filename}-final.mp4"
+
+    hevc_upscale $source $upscaled
+    comparison_render $source $upscaled $final
 done
