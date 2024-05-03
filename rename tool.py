b4c65ec0c94c13a6267f87e749a32d9f600fde80
JDJG <167665838+JDJG3493@users.noreply.github.com>
2024-05-03 18:21:09 -0400

    Made ffmpeg shengians be async and wait for stuff.
    This may require an order to exist but unsure.
    
    It does print what it is running though.

--- rename tool.py
+++ rename tool.py
@@ -1,11 +1,10 @@
 import pathlib
-import os
 import datetime
 
-# Get the path to the current directory
-current_directory = pathlib.Path.cwd()
+# Get the path to the current location of where the script is stored
+current_directory = pathlib.Path(__file__).absolute().parent
 files = list(current_directory.rglob("*.mp4"))
-files.sort(key=lambda x: os.path.getmtime(x))
+files.sort(key=lambda path: path.stat().st_mtime)
 
 count = 0
 num = int(input("enter the video number:"))
@@ -17,6 +16,7 @@ for file in files:
         if file.suffix == ".mp4":
             stat = file.stat()
             ok = datetime.datetime.fromtimestamp(stat.st_mtime).strftime("%c")
+            # ok not used
 
             file.rename(f"video-{num}.mp4")
             print(f"renamed file to video-{num}.mp4")
--- upconvert.py
+++ upconvert.py
@@ -1,58 +1,61 @@
+import asyncio
 import pathlib
-import os
 import datetime
-import subprocess
 
 
 # Get the path to the current directory
-current_directory = pathlib.Path.cwd()
+current_directory = pathlib.Path(__file__).absolute().parent
 files = list(current_directory.rglob("*.mp4"))
-files.sort(key=lambda x: os.path.getmtime(x))
+files.sort(key=lambda path: path.stat().st_mtime)
 
 
-# List all files in the current directory
-for file in files:
-    if file.is_file():
-        name = file.name
+async def ffmpeg_shengians():
+    # List all files in the current directory
+    for file in files:
+        if file.is_file():
+            name = file.name
 
-        if file.suffix == ".mp4":
-            stat = file.stat()
-            ok = datetime.datetime.fromtimestamp(stat.st_mtime).strftime("%c")
+            if file.suffix == ".mp4":
+                stat = file.stat()
+                ok = datetime.datetime.fromtimestamp(stat.st_mtime).strftime("%c")
+                # unused ok variable may use it for prints or something else I don't know yet.
 
-            source_file = str(file.name)
-            base_file = f"{file.stem} upgraded.mp4"
-            quality_file = f"{file.stem} quality.mp4"
-
-            subprocess.check_call(
-                [
-                    "ffmpeg",
-                    "-i",
-                    source_file,
-                    "-c:v",
-                    "hevc_nvenc",
-                    "-qp",
-                    "17",
-                    "-vf",
-                    "scale=1920x1080:flags=lanczos",
-                    base_file,
-                ]
-            )
-            subprocess.check_call(
-                [
-                    "ffmpeg",
-                    "-i",
-                    source_file,
-                    "-i",
-                    base_file,
-                    "-c:v",
-                    "hevc_nvenc",
-                    "-qp",
-                    "17",
-                    "-filter_complex",
-                    "[0]pad=iw:1080:0:(1080-ih)/2,hstack",
-                    quality_file,
-                ]
-            )
+                source_file = str(file)
+                base_file = file.with_stem(f"{file.stem} upgraded")
+                quality_file = file.with_stem(f"{file.stem} quality")
 
 
+                proc = await asyncio.create_subprocess_exec(
+                        "ffmpeg",
+                        "-i",
+                        source_file,
+                        "-c:v",
+                        "hevc_nvenc",
+                        "-qp",
+                        "17",
+                        "-vf",
+                        "scale=1920x1080:flags=lanczos",
+                        base_file,
+                )
+                await proc.wait()
+
+                proc = await asyncio.create_subprocess_exec(
+                        "ffmpeg",
+                        "-i",
+                        source_file,
+                        "-i",
+                        base_file,
+                        "-c:v",
+                        "hevc_nvenc",
+                        "-qp",
+                        "17",
+                        "-filter_complex",
+                        "[0]pad=iw:1080:0:(1080-ih)/2,hstack",
+                        quality_file,
+                )
+
+                await proc.wait()
+
+
+asyncio.run(ffmpeg_shengians())
 print("done")
